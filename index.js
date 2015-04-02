31d4ab7d81f3dca1eaf656bcfc42d99068330378
Qingye Yuan <qingye.yuan@gmail.com>
2015-04-02 07:08:36 +0000

    template push

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var fs = require('fs')
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-var buf = fs.readFileSync("index.html")
+var buf = fs.readFileSync("bitstarter.html")
 
 app.get('/', function(request, response) {
   response.send(buf.toString('ascii'))
