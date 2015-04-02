d6b98bb12993c49d97ff8db0c711381606c63491
Qingye Yuan <qingye.yuan@gmail.com>
2015-04-02 06:26:50 +0000

    change for the third time

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express();
+var fs = require('fs')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var buf = rs.readFileSync("index.html")
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!')
+  response.send(buf.toString('ascii'))
 })
 
 app.listen(app.get('port'), function() {
