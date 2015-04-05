ee853b54ace477148bb3926e2845f1a0c3bdfc17
Qingye Yuan <qingye.yuan@gmail.com>
2015-04-05 20:22:19 +0000

    change

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express();
 var fs = require('fs')
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 var buf = fs.readFileSync("bitstarter.html")
